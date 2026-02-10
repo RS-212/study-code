b85c59b434240517aabefc2e772deb7ae698f429
RS-212 <roman.soell+github@gmail.com>
2026-02-10 18:33:57 +0100

    Section I

---
+++ courses/coding-in-C/Lab_3/mySolution/calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    float f_num1 = 0;
+    float f_num2 = 0;
+    char c_operator = ' ';
+    float f_result = 0;
+
+    do
+    {
+        printf("Input the first floating point number...\n\n");
+    } while (!scanf("%f", &f_num1));
+    do
+    {
+        printf("\nInput the second floating point number...\n\n");
+    } while (!scanf("%f", &f_num2));
+
+    while (1)
+    {
+        printf("\nWhich operation? add(+), subtract(-), multiply(*), divide(/)\n\n");
+        scanf(" %c", &c_operator);
+
+        // Check division by zero
+        if (c_operator == '/' && f_num2 == 0)
+        {
+            printf("\nYou cannot divide by zero! Try again...\n");
+            continue;
+        }
+        // Check validity of operator
+        if (c_operator == '+' || c_operator == '-' || c_operator == '*' || c_operator == '/')
+        {
+            break;
+        }
+        printf("\nInvalid operation! Try again...\n\n");
+    }
+
+    // Execute calculation
+    if (c_operator == '+')
+    {
+        f_result = f_num1 + f_num2;
+    } else if (c_operator == '-')
+    {
+        f_result = f_num1 - f_num2;
+    } else if (c_operator == '*')
+    {
+        f_result = f_num1 * f_num2;
+    } else if (c_operator == '/')
+    {
+        f_result = f_num1 / f_num2;
+    }
+
+    // Print result
+    printf("\n%f %c %f = %f\n", f_num1, f_num2, c_operator, f_result);
+
+    return 0;
+}
