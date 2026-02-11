abde7a35a766fd1c8a756e5fbe659037102c7dd9
RS-212 <roman.soell+github@gmail.com>
2026-02-11 20:08:06 +0100

    fixed palindromes

--- courses/coding-in-C/Lab_4/mySolution/Section_II.c
+++ courses/coding-in-C/Lab_4/mySolution/Section_II.c
@@ -2,35 +2,22 @@
 #include <stdbool.h>
 #include <string.h>
 
-int get_string_length(char ca_input[])
-{
-    int length = 0;
-    while (true)
-    {
-        if (ca_input[length] == '\0')
-        {
-            break;
-        }
-        length++;
-    }
-    return length;
-}
+#define MAX_STR_LEN 100
 
 /**
  * @brief checks if a given word is a palindrome
  * 
  * @param input string to check
- * @param length length of the string
  * 
  * @return returns 1 if input is a palindrome. returns 0 if input is not a palindrome.
  */
 bool check_palindrome(char ca_input[])
 {
-    const int i_length = get_string_length(ca_input);
-    const int i_middle = i_length / 2;     // rounds down in case of uneven numbers -> middle letter is ignored
+    int i_length = strlen(ca_input) - 1;    // -1 to ignore \0
+    int i_middle = i_length / 2;        // rounds down in case of uneven numbers -> middle letter is ignored
     for (int i = 0; i < i_middle; i++)
     {
-        printf("\n%c\n%c\n", ca_input[i], ca_input[i_length-i]);
+        //printf("\n%c\n%c\n", ca_input[i], ca_input[i_length-i]);
         if (ca_input[i] != ca_input[i_length-i])
         {
             return false;
@@ -41,20 +28,31 @@ bool check_palindrome(char ca_input[])
 
 int main()
 {
-    char ca_user_input[101] = {0};
+    char ca_user_input[MAX_STR_LEN+2] = "";      // Read string can be max. 100 chars -> +2 for \0 and \n
 
-    printf("Please enter a word (max. 100 characters): ");
-    fgets(ca_user_input, sizeof(ca_user_input), stdin);
-
-    //printf("%s", ca_user_input);
-
-    // Check for palindrome
-    printf("Your provided word \"%s\" is ", ca_user_input);
-    if (!check_palindrome(ca_user_input))
+    // Try to read in word. Retry if it fails
+    while (true)
     {
-        printf("not");
+        printf("Please enter a word (max. 100 characters): ");
+        if (fgets(ca_user_input, sizeof(ca_user_input), stdin))
+        {
+            break;
+        }
+        printf("Failed to read word. Try again...\n");
     }
-    printf(" a palindrome.\n");
+
+    // Remove \n from the end of the word
+    char ca_edited_input[MAX_STR_LEN+1] = "";
+    strncat(ca_edited_input, ca_user_input, strlen(ca_user_input)-1);
+
+    if (check_palindrome(ca_edited_input))
+    {
+        printf("Your provided word \"%s\" is a palindrome", ca_edited_input);
+    }
+    else
+    {
+        printf("Your provided word \"%s\" is not a palindrome", ca_edited_input);
+    }
 
     return 0;
 }
