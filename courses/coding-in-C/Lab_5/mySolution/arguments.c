fe50f02e51b6ea2f6bf2b7356e9911964564946a
Roman Söll <144619163+RS-212@users.noreply.github.com>
2026-02-12 13:34:12 +0000

    complete Lab_5 Section I and II

---
+++ courses/coding-in-C/Lab_5/mySolution/arguments.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    if (!(argc > 1))
+    {
+        printf("This program expects at least one(1) argument!\nExiting...\n");
+        return -1;
+    }
+    
+    //for (int i = 1; i < (sizeof(argv) / sizeof(*argv)); i++)
+    for (int i = 1; i < argc; i++)
+    {
+        char *argument = argv[i];
+        //char *argument = *(argv + i);
+        
+        printf("Argument %d at address \"%p\" has content: %s (length: %d)\n", i, argument, argument, (int)strlen(argument));
+    }
+
+    /*
+    5. arr[i] == *(arr + i) -> Because arrays contain the address of their first element, they can be handled like pointers
+    6. Arrays are pointers to their first element, with additional information about length (sizeof(arr)).
+    */
+
+    return 0;
+}
