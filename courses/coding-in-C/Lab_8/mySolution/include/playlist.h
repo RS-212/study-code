d76bb7dd882c05f273e75512477ab866772a7c95
RS-212 <roman.soell@gmail.com>
2026-03-05 15:33:13 +0100

    work on Lab_8

---
+++ courses/coding-in-C/Lab_8/mySolution/include/playlist.h
@@ -0,0 +1,27 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
+#define MAX_SONGS 100
+
+// Structs
+typedef struct Song
+{
+    char *title;
+    char *artist;
+    struct Song *next_song;
+} Song;
+
+typedef struct
+{
+    Song *first_song;
+} Playlist;
+
+// Function prototypes
+Playlist *init_playlist();
+int add_song(const char *title, const char *artist, Playlist *playlist);
+void print_playlist(Playlist *playlist);
+int delete_firstSong(Playlist *playlist);
+int delete_playlist(Playlist *playlist);
+int get_playlist_length(Playlist *playlist);
+
+#endif
---
+++ courses/coding-in-C/Lab_8/mySolution/main.c
@@ -0,0 +1,36 @@
+#include "./include/playlist.h"
+#include <stdio.h>
+
+int main()
+{
+    const char *songs[] = {"Daydreamers", "Song 2", "Stick Up", "Moonshine"};
+    const char *artists[] = {"AURORA", "Blur", "Grandson", "Caravan Palace"};
+
+    // 1. Create and initialize a playlist
+    Playlist *my_playlist = init_playlist();
+
+    // 2. Add songs to playlist
+    for(int i = 0; i < 4; i++)
+    {
+        if(!add_song(songs[i], artists[i], my_playlist))
+        {
+            printf("Error adding song. Exiting!\n");
+            delete_playlist(my_playlist);
+            return 1;
+        }
+    }
+
+    // 3. Print the playlist
+    print_playlist(my_playlist);
+
+    // 4. Delete the first song
+    delete_firstSong(my_playlist);
+
+    // 5. Print playlist again
+    print_playlist(my_playlist);
+
+    // 6. Delete complete playlist and free memory for playlist
+    delete_playlist(my_playlist);
+
+    return 0;
+}
